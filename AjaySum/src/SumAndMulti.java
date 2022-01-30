9bd89c47ab91caef41ed1e71dface408a76eca72
ajay <ajaysingh6531@gmail.com>
2022-01-30 14:37:18 +0530

    both function added

--- AjaySum/src/SumAndMulti.java
+++ AjaySum/src/SumAndMulti.java
@@ -1,9 +1,22 @@
-
+import java.util.Scanner;
 public class SumAndMulti {
-
+	
+	public static int sum() {
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		return sum;
+	}
+	public static int multi() {
+		return 10*20;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner s=new Scanner(System.in);
+		int sumres=sum();
+		System.out.println(sumres);
+		int multires=multi();
+		System.out.println(multires);
 	}
 
 }
